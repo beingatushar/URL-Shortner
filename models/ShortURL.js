1154e34893da4442eed8ef66eb78caeeebbcadb9
beingatushar <beingatushar@gmail.com>
2023-01-12 00:59:56 +0530

    bug fix

--- models/ShortURL.js
+++ models/ShortURL.js
@@ -1,15 +1,32 @@
-const mongoose = require('mongoose');
-const ShortURLSchema = mongoose.Schema({
-    url: {
+const mongoose = require('mongoose')
+const shortId = require('shortid')
+const shortUrlSchema = new mongoose.Schema({
+    full: {
         type: String,
         required: true
     },
-    shortURL: {
+    short: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        default: shortId.generate
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: new Date(),
+        unmodifiable: true
+
+    },
+    lastVisitedAt: {
+        type: Date,
+        required: true,
+        default: new Date()
     },
     clicks: {
-        type: Number
+        type: Number,
+        required: true,
+        default: 0
     }
-});
-module.exports = new mongoose.model('ShortURL', ShortURLSchema);
+})
+module.exports = mongoose.model('ShortUrl', shortUrlSchema);
